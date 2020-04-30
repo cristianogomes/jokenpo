1bdc36aeaf575e3acdc12c0f8ab44966e9c1d207
Cristiano Gomes <cristianno@ymail.com>
2020-04-30 13:32:30 -0300

    Iniciando projeto

---
+++ src/main/java/com/jokenpo/game/JokenpoGameApplication.java
@@ -0,0 +1,13 @@
+package com.jokenpo.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokenpoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokenpoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokenpo/game/JokenpoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokenpo.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokenpoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
