ccf1bba0169a210d3aa61db50cabf4400cf5c26c
Cristiano Gomes <cristianno@ymail.com>
2020-05-06 20:24:58 -0300

    Ajusta metodo delete ToolService

--- src/main/java/com/jokenpo/game/repository/ActionRepository.java
+++ src/main/java/com/jokenpo/game/repository/ActionRepository.java
@@ -1,9 +1,18 @@
 package com.jokenpo.game.repository;
 
 import com.jokenpo.game.model.Action;
+import com.jokenpo.game.model.Tool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActionRepository extends JpaRepository<Action, Long> {
+
+    @Modifying
+    @Query("DELETE FROM Action WHERE source = :tool OR target = :tool")
+    void deleteActionByTool(@Param("tool") Tool tool);
+
 }
--- src/main/java/com/jokenpo/game/service/impl/ToolServiceImpl.java
+++ src/main/java/com/jokenpo/game/service/impl/ToolServiceImpl.java
@@ -1,11 +1,31 @@
 package com.jokenpo.game.service.impl;
 
+import com.jokenpo.game.exception.NotFoundException;
 import com.jokenpo.game.model.Tool;
+import com.jokenpo.game.repository.ActionRepository;
 import com.jokenpo.game.repository.ToolRepository;
 import com.jokenpo.game.service.ToolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ToolServiceImpl extends AbstractServiceImpl<Long, Tool, ToolRepository> implements ToolService {
+
+    @Autowired
+    private ToolRepository toolRepository;
+
+    @Autowired
+    private ActionRepository actionRepository;
+
+    @Transactional
+    public void delete(Long id) throws NotFoundException {
+        Tool tool = this.findById(id);
+
+        this.actionRepository.deleteActionByTool(tool);
+        tool.setActions(null);
+        this.toolRepository.delete(tool);
+    }
 
 }
